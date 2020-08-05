4bacfcee031a043a2860c81d953d1204d6b21e57
dasom <dasom.choi08@gmail.com>
2020-08-05 16:09:26 +0900

    first commit

---
+++ anotherPrac/ref.js
@@ -0,0 +1,43 @@
+//공식홈 
+// https://create-react-app.dev/docs/deployment
+//https://ko.reactjs.org/docs/create-a-new-react-app.html
+
+// axois 사용
+// https://www.youtube.com/watch?v=kJA9rDX7azM
+
+//반응형 웹강좌
+// https://www.youtube.com/watch?v=vfN5_OK-bXY&list=PL4UVBBIc6giL7ygRa-P7UExEKqZgx4t9K&index=10
+
+//css animation
+//https://www.youtube.com/watch?v=k7I1K68IrEo
+
+//레드독 웹사이트
+// http://reddogch.com/
+
+//반응형 웹사이트 모음
+//http://rwdb.kr/global/
+
+//리액트 레이아웃 툴
+//https://medium.com/querypie/react%EB%A1%9C-%EB%A7%8C%EB%93%9C%EB%8A%94-electron-%EC%9B%B9-%EC%95%A0%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98-%EB%A0%88%EC%9D%B4%EC%95%84%EC%9B%83-antd-customization-817bb2cf1d4b
+
+//구글 웹폰트 적용하기
+//https://uxgjs.tistory.com/156
+
+//flex박스 
+//https://www.youtube.com/watch?v=fYq5PXgSsbE
+
+//rem
+//https://yeoninim.tistory.com/38
+
+//모바일 해상도
+// https://brunch.co.kr/@plusx/6
+
+//이미지 편집 웹사이트
+// https://m.blog.naver.com/gbckl/221262909590
+// https://pixlr.com/kr/x/
+
+//어떤 기술 스택을 사용중인지 나타냄
+//https://stackshare.io/airbnb/airbnb
+
+//반응형 작업시 이미지 고정
+// https://code-study.tistory.com/13
